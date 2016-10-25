271ed83c39cceb4aaabdfe0b290d58478be1aa3b
Kelvin L <Kelvin L>
2016-10-25 12:47:50 -0400

    Initial Commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,13 @@
+const ohHey = "Hello World";
+
+console.log('hii');
+console.log(ohHey);
+
+const numger = [1,2,3];
+const maxNum = Math.max(...numbers);
+
+var string = `ths is a ${ohHey}`;
+
+console.log(string);
+
+console.log('hello');
---
+++ public/scripts/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var ohHey = "Hello World";
+
+console.log('hii');
+console.log(ohHey);
+
+var numger = [1, 2, 3];
+var maxNum = Math.max.apply(Math, _toConsumableArray(numbers));
+
+var string = 'ths is a ' + ohHey;
+
+console.log(string);
+
+console.log('hello');
